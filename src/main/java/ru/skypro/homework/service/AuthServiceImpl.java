835669b739e0d5424aa7ab70a4936c68df6d4ad9
https://github.com/enot-therock/if_else.git <enot.therock@gmail..com>
2025-09-20 11:40:25 +0300

    step_3.1

--- src/main/java/ru/skypro/homework/service/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/AuthServiceImpl.java
@@ -1,4 +1,4 @@
-package ru.skypro.homework.service.auth;
+package ru.skypro.homework.service;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
